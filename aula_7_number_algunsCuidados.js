bae4c79047832b2a8902ca3a4ee5572d015024db
Mayara Pinheiro <67695977+maypinheiro@users.noreply.github.com>
2020-07-29 16:36:55 -0300

    Number

---
+++ aula_7_number_algunsCuidados.js
@@ -0,0 +1,23 @@
+/* Number alguns cuidados  */
+
+console.log( 7/ 0)  // retorna Infinito
+console.log('10' / 2) // realiza a operação mesmo sendo uma string
+console.log("10.9" / 2) // realiza a operação , pois ele tenta se aproximar
+console.log("casa" * 2) // retorna NaN 
+console.log(3.745 +3.743 ) //Não retorna número decimal exato.
+                       
+console.log((18.49384).toFixed(2))  // PODEMOS CHAMAR A FUNÇÃO DESTA FORMA
+console.log((10).toFixed(2))
+
+console.log(10.toFixed(2)) 
+ // nos retorna um erro. Não podemos chamr um literal e depois a função
+
+
+
+
+
+
+
+
+
+ 
