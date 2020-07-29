92cc04eebac1fb57b97f9f1f687973245e3d699e
Mayara Pinheiro <67695977+maypinheiro@users.noreply.github.com>
2020-07-29 10:01:11 -0300

    Anotaçoes
    Algoritmo, Estrutura de dados

---
+++ Aulas/Algaritmo.js
@@ -0,0 +1,41 @@
+// Algoritmo 
+// Sequencia de passos lógicos, estruturados e finitos que visam executar uma ação.
+// Suporta repetição.
+// Posso Repetir x vezes
+// Posso repetir ate ...
+// Pode ser sequêncial.
+// Tb é possivel tomar uma decisão.
+// A ordem dos passos é importante.
+// Tem algaritmos que tem: Entreda e saida , Não tem entrada mas tem saida, não tem entrada e nem saida.
+// Os algoritmos podem ser representados em :
+// Fluxograma, linguagem natural, artificial (java, js, c#...), pseudo-linguagem.
+// Alto nivel: Mais Proxima da linguagem natural - Baixo Nivel: Mais próximo da linguagem de Maquina.
+
+
+//     ESTRUTURA DE DADOS.
+
+// Visa Organizar e Administrar os dados.
+// Da as regras de como os dados precisam se comportar.
+// Estrutura FIFO - Primeiro a entrar é o Primeiro a sair.
+// Estrutura LIFO - Primeiro a entrar é o Último a sair.
+// Estrutura de ARVORE
+// Estrutura de TABELA.
+// Estrutura Básicas - Int, real, letra, bool.
+
+
+// VARIÁVEIS E CONSTANTES.
+
+//Servem para armazenar dados. 
+//Constantes - valores que não se alteram.
+//Variáveis - valores que se alteram. 
+
+
+
+
+
+ 
+
+
+
+
+
