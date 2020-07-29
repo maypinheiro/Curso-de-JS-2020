5f7195173323a49ee7662e848d93ff804c61fa03
Mayara Pinheiro <67695977+maypinheiro@users.noreply.github.com>
2020-07-29 11:43:12 -0300

    Ferramentas Online e Comentários
    Ferramentas Online e Comentários

---
+++ Aulas/aula_3_ferramentas.js
@@ -0,0 +1,29 @@
+// Ferramentas Online para executar JS
+
+//Rhttps://repl.it/languages/javascript
+//https://jsfiddle.net/
+
+ 
+// COmentário de Código.
+
+// Não é interpletado.
+// Não faça comentários desnecessário.
+// Faça códigos legiveis.
+// Faça codigos limpos.
+
+//Comentário em 1 linha 
+console.log(`Linha 1`)
+
+/* Comentário em 
+multiplas 
+linhas
+*/
+console.log(`Linha 2`)
+
+/*
+ * Coméntário
+ * múltiplaas 
+ * Linhas
+*/
+console.log(`Linha 3`)
+
