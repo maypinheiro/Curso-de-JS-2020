84a41d99668f2d9937800744df60ec9d16a13a57
Mayara Pinheiro <67695977+maypinheiro@users.noreply.github.com>
2020-07-29 11:04:22 -0300

    Fundamentos
    ESTRUTURA DE DADOS., organização Básica de um código em JS

---
+++ Aulas/aula_2_fundamentos.js
@@ -0,0 +1,23 @@
+//organização Básica de um código em JS
+
+// O JS é organizado em sentenças de código , que podem ou não terminar em ; podem ou não ocupar mais de uma linha.
+// Também se organizam em Blocos {} . E dentro de um bloco podemos abrir ou não outros blocos.
+// Não podemos fazer interseção entre blocos.
+// Podemos também abrir blocos vazios.
+
+console.log(`Olá Mundo!`) //Sentença de Código.
+
+
+{
+    console.log(`Olá Mundo!`) //Bloco de Código.
+}
+
+{
+    //sentença vazia
+}
+
+{
+    {
+        console.log(`Olá mundo!`) // Sentença de código dentro de outra sentença.
+    }
+}
