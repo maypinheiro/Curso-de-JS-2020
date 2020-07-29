47b252ea1d4c7398e051b37b208b696e93593b00
Mayara Pinheiro <67695977+maypinheiro@users.noreply.github.com>
2020-07-29 16:11:42 -0300

    Number

---
+++ aula_6_number.js
@@ -0,0 +1,39 @@
+// Number 
+/*   typeof verifica o tipo de dado */
+
+/* Number Representa uma função. 
+number representa um numero.
+O tipo number serve para inteiro e decimal
+podemos atribuir algumas funcionalidades 
+usamos . 
+.isInteger()   verificar se o valor é inteiro
+.toString()    Transformar o Numero em binario
+.toFixed()     Determinar a quantidade de casas decimais
+colocamos ele colado ao  */
+
+const peso1 = 1.0   // podemos declar assim  
+const peso2 = Number('2.0') // e assim
+console.log (peso1, peso2)
+console.log (typeof peso1,typeof peso2)
+console.log(Number.isInteger(peso1))
+console.log(Number.isInteger(peso2))
+
+// Para sabermos se o numero é inteiro. (Number.isInteger())
+
+const av1 = 9.876
+const av2 = 5.987
+
+const total = av1 * peso1 + av2 * peso2
+const media = total / (peso1 +peso2)
+
+console.log(media.toFixed(2))
+//toFixed é usado para determinar quantas casas decimais desejo atos o .
+
+console.log(media.toString())
+//toString transforma um valor em Binario.
+console.log(typeof media)
+console.log(typeof Number)
+
+
+
+
